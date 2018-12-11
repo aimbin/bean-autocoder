e8440ffbdba8458cc3604545d26c5eb655a071b0
aimbin <fun_endless@163.com>
2018-12-12 00:50:35 +0800

    Can create bean with setter and getter and import.
    Signed-off-by: aimbin <fun_endless@163.com>

--- autocoder/org.aimbin.autocoder.bean/src/main/java/org/aimbin/autocoder/component/ClassImports.java
+++ autocoder/org.aimbin.autocoder.bean/src/main/java/org/aimbin/autocoder/component/ClassImports.java
@@ -1,27 +1,52 @@
 /** fun_endless@163.com  2018年12月8日 */
 package org.aimbin.autocoder.component;
 
-import java.util.List;
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  * @author aimbin
  * @verison 1.0.0 2018年12月8日
  */
 public class ClassImports {
-	private List<String> imports;
+	private Set<String> imports = null;
 
 	/**
 	 * @return the imports
 	 */
-	public List<String> getImports() {
+	public Set<String> getImports() {
 		return imports;
 	}
 
 	/**
 	 * @param imports the imports to set
 	 */
-	public void setImports(List<String> imports) {
+	public void setImports(Set<String> imports) {
 		this.imports = imports;
+	}
+	
+	/**contains */
+	public boolean contains(String fullType) {
+		if(getImports() == null) {
+			return false;
+		}
+		return getImports().contains(fullType);
+	}
+	
+	/**Add an import, skip not-need. */
+	public boolean addImport(Class<?> fullType) {
+		if(imports == null) {
+			imports = new HashSet<>();
+		}
+		if(fullType.isPrimitive() || fullType.getName().startsWith("java.lang.")) {
+			//No need declare import.
+			return false;
+		}else if(imports.contains(fullType.getName())) {
+			return false;
+		}else {
+			imports.add(fullType.getName());
+			return true;
+		}
 	}
 	
 	
